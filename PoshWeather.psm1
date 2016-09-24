9d5f94e4d45a2363a220250ad9a98d6d18d5faa0
Martin Howlett <martin.howlett@gmail.com>
2016-09-24 21:24:29 +0100

    Added get-weather and moved test-umbrellanedded to use that function

--- PoshWeather.psm1
+++ PoshWeather.psm1
@@ -1,24 +1,24 @@
 ﻿<#
     .Synopsis
-       Returns if it is raining or not
+        Returns if it is raining or not
 
     .DESCRIPTION
-       Queries the openweathermap API and returns if it raining or not
-       based on city code
+        Queries the openweathermap API and returns if it raining or not
+        based on city code
 
     .PARAMETER ApiKey
-       API key for openweathermap
-       Get one from http://openweathermap.org/appid#get
-
+        API key for openweathermap
+        Get one from http://openweathermap.org/appid#get
+    
     .PARAMETER Cityname
-        The name of the city to get the weather for
+       The name of the city to get the weather for
 
     .PARAMETER Countrycode
         The Country code the city is in
         See here: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements
 
     .EXAMPLE
-       Test-UmbrellaNeeded -ApiKey "e48gy9j4de" -Cityname "london" -Countrycode "uk"
+       Test-UmbrellaNeeded -ApiKey "f0b35185ec994312bff9c29221f360a7" -Cityname "london" -Countrycode "uk"
 
     .OUTPUTS
        True or False
@@ -35,11 +35,7 @@ Function Test-UmbrellaNeeded
         [Parameter(Mandatory=$true)][string]$Countrycode
     )
 
-    #create the url from the parameters
-    $uri = "http://api.openweathermap.org/data/2.5/weather?q=$Cityname,$Countrycode&appid=$ApiKey"
-
-    #query the API
-    $Response = Invoke-RestMethod -uri $uri
+    $Response = Get-Weather -ApiKey $ApiKey -Cityname $Cityname -Countrycode $Countrycode
 
     #check if its raining  and return true/false
     if($Response.rain)
@@ -50,4 +46,50 @@ Function Test-UmbrellaNeeded
     {
        Write-Output $false
     }
+}
+
+<#
+    .Synopsis
+        Gets current weather of provided city
+
+    .DESCRIPTION
+        Queries the OpenWeatherMap API with the current city and returns the current weather
+
+    .PARAMETER ApiKey
+        API key for openweathermap
+        Get one from http://openweathermap.org/appid#get
+
+    .PARAMETER Cityname
+        The name of the city to get the weather for
+
+    .PARAMETER Countrycode
+        The Country code the city is in
+        See here: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements
+
+    .EXAMPLE
+       Get-Weather -ApiKey "f0b35185ec994312bff9c29221f360a7" -Cityname "london" -Countrycode "uk"
+
+    .OUTPUTS
+       Psobject of weather
+
+    .NOTES
+       Written by Martin Howlett @WillCode4Pizza
+#>
+Function Get-Weather
+{
+    Param (
+
+        [Parameter(Mandatory=$true)][string]$ApiKey,
+        [Parameter(Mandatory=$true)][string]$Cityname,
+        [Parameter(Mandatory=$true)][string]$Countrycode
+    )
+
+    #create the url from the parameters
+    $uri = "http://api.openweathermap.org/data/2.5/weather?q=$Cityname,$Countrycode&appid=$ApiKey"
+
+    #query the API
+    $Response = Invoke-RestMethod -uri $uri
+
+    #return response
+    Return $Response
 }
